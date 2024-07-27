6dbcce66380f17bf9145d27ed2e1c75f25e6fffd
RikaYoshida2024 <spandanpatnaik123@gmail.com>
2024-07-27 21:05:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Reimu Hakurei";
+            Console.WriteLine(phrase.Substring(phrase.IndexOf("Hakurei")));
+
+        }
+    }
+}
